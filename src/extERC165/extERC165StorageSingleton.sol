27957d4522d7fb7e55f98d6299550c5e78925c61
YamenMerhi <yamennmerhi@gmail.com>
2024-07-27 02:48:29 +0300

    Add ExtERC165StorageSingleton extension

---
+++ src/extERC165/extERC165StorageSingleton.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+
+/**
+ * @title ExtERC165StorageSingleton
+ * @dev Implementation of the ERC165 standard for interface detection, with extended functionality.
+ * This contract allows each address to manage their own interface support independently.
+ */
+contract ExtERC165StorageSingleton is IERC165 {
+    /// @dev Mapping to store interface support for each address
+    mapping(address => mapping(bytes4 => bool)) private _supportedInterfaces;
+
+    /**
+     * @param interfaceId The interface identifier, as specified in ERC-165
+     * @return bool True if the contract supports `interfaceId`, false otherwise
+     * @notice This function checks if the caller supports the given interface
+     */
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override returns (bool) {
+        return _supportedInterfaces[msg.sender][interfaceId];
+    }
+
+    /**
+     * @dev Adds support for an interface for the calling address.
+     * @param interfaceId The interface identifier, as specified in ERC-165
+     */
+    function addSupport(bytes4 interfaceId) public {
+        _addSupport(msg.sender, interfaceId);
+    }
+
+    /**
+     * @dev Removes support for an interface for the calling address.
+     * @param interfaceId The interface identifier, as specified in ERC-165
+     */
+    function removeSupport(bytes4 interfaceId) public {
+        _supportedInterfaces[msg.sender][interfaceId] = false;
+    }
+
+    /**
+     * @dev Internal function to add support for an interface.
+     * @param account The address for which to add support
+     * @param interfaceId The interface identifier, as specified in ERC-165
+     * @notice This function is used internally to add interface support for a specific address
+     */
+    function _addSupport(address account, bytes4 interfaceId) internal {
+        _supportedInterfaces[account][interfaceId] = true;
+    }
+}
---
+++ test/extERC165StorageSingleton.t.sol
@@ -0,0 +1,303 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import "../src/Extendable.sol";
+import "../src/extERC165/ExtERC165StorageSingleton.sol";
+
+contract ExtERC165StorageSingletonTest is Test {
+    ExtERC165StorageSingleton public erc165Extension;
+    Extendable public extendable1;
+    Extendable public extendable2;
+    Extendable public extendable3;
+    address public owner;
+
+    bytes4 constant ERC165_INTERFACE_ID = type(IERC165).interfaceId;
+    bytes4 constant SUPPORTS_INTERFACE_SELECTOR =
+        IERC165.supportsInterface.selector;
+    bytes4 constant ADD_SUPPORT_SELECTOR =
+        bytes4(keccak256("addSupport(bytes4)"));
+    bytes4 constant REMOVE_SUPPORT_SELECTOR =
+        bytes4(keccak256("removeSupport(bytes4)"));
+
+    function setUp() public {
+        owner = address(this);
+        erc165Extension = new ExtERC165StorageSingleton();
+        extendable1 = new Extendable(owner);
+        extendable2 = new Extendable(owner);
+        extendable3 = new Extendable(owner);
+    }
+
+    function testAddingExtension() public {
+        extendable1.addExtension(
+            SUPPORTS_INTERFACE_SELECTOR,
+            address(erc165Extension)
+        );
+        assertFalse(
+            IERC165(address(extendable1)).supportsInterface(ERC165_INTERFACE_ID)
+        );
+    }
+
+    function testAddSupport() public {
+        extendable1.addExtension(
+            SUPPORTS_INTERFACE_SELECTOR,
+            address(erc165Extension)
+        );
+
+        extendable1.execute(
+            address(erc165Extension),
+            abi.encodeWithSelector(ADD_SUPPORT_SELECTOR, ERC165_INTERFACE_ID)
+        );
+
+        assertTrue(
+            IERC165(address(extendable1)).supportsInterface(ERC165_INTERFACE_ID)
+        );
+        vm.expectRevert("No extension found");
+        IERC165(address(extendable2)).supportsInterface(ERC165_INTERFACE_ID);
+    }
+
+    function testRemoveSupport() public {
+        extendable1.addExtension(
+            SUPPORTS_INTERFACE_SELECTOR,
+            address(erc165Extension)
+        );
+
+        extendable1.execute(
+            address(erc165Extension),
+            abi.encodeWithSelector(ADD_SUPPORT_SELECTOR, ERC165_INTERFACE_ID)
+        );
+        assertTrue(
+            IERC165(address(extendable1)).supportsInterface(ERC165_INTERFACE_ID)
+        );
+
+        extendable1.execute(
+            address(erc165Extension),
+            abi.encodeWithSelector(REMOVE_SUPPORT_SELECTOR, ERC165_INTERFACE_ID)
+        );
+        assertFalse(
+            IERC165(address(extendable1)).supportsInterface(ERC165_INTERFACE_ID)
+        );
+    }
+
+    function testMultipleExtendableSupport() public {
+        extendable1.addExtension(
+            SUPPORTS_INTERFACE_SELECTOR,
+            address(erc165Extension)
+        );
+        extendable2.addExtension(
+            SUPPORTS_INTERFACE_SELECTOR,
+            address(erc165Extension)
+        );
+
+        bytes4 customInterfaceId = bytes4(keccak256("customInterface()"));
+
+        extendable1.execute(
+            address(erc165Extension),
+            abi.encodeWithSelector(ADD_SUPPORT_SELECTOR, ERC165_INTERFACE_ID)
+        );
+        extendable2.execute(
+            address(erc165Extension),
+            abi.encodeWithSelector(ADD_SUPPORT_SELECTOR, customInterfaceId)
+        );
+
+        assertTrue(
+            IERC165(address(extendable1)).supportsInterface(ERC165_INTERFACE_ID)
+        );
+        assertFalse(
+            IERC165(address(extendable1)).supportsInterface(customInterfaceId)
+        );
+
+        assertFalse(
+            IERC165(address(extendable2)).supportsInterface(ERC165_INTERFACE_ID)
+        );
+        assertTrue(
+            IERC165(address(extendable2)).supportsInterface(customInterfaceId)
+        );
+    }
+
+    function testRemovingExtension() public {
+        extendable1.addExtension(
+            SUPPORTS_INTERFACE_SELECTOR,
+            address(erc165Extension)
+        );
+        extendable1.execute(
+            address(erc165Extension),
+            abi.encodeWithSelector(ADD_SUPPORT_SELECTOR, ERC165_INTERFACE_ID)
+        );
+
+        extendable1.removeExtension(SUPPORTS_INTERFACE_SELECTOR);
+
+        vm.expectRevert("No extension found");
+        IERC165(address(extendable1)).supportsInterface(ERC165_INTERFACE_ID);
+    }
+
+    function testBeforeAddingExtension() public {
+        vm.expectRevert("No extension found");
+        IERC165(address(extendable1)).supportsInterface(ERC165_INTERFACE_ID);
+    }
+
+    function testAddSupportForMultipleInterfaces() public {
+        extendable1.addExtension(
+            SUPPORTS_INTERFACE_SELECTOR,
+            address(erc165Extension)
+        );
+
+        bytes4 customInterface1 = bytes4(keccak256("customInterface1()"));
+        bytes4 customInterface2 = bytes4(keccak256("customInterface2()"));
+
+        extendable1.execute(
+            address(erc165Extension),
+            abi.encodeWithSelector(ADD_SUPPORT_SELECTOR, customInterface1)
+        );
+        extendable1.execute(
+            address(erc165Extension),
+            abi.encodeWithSelector(ADD_SUPPORT_SELECTOR, customInterface2)
+        );
+
+        assertTrue(
+            IERC165(address(extendable1)).supportsInterface(customInterface1)
+        );
+        assertTrue(
+            IERC165(address(extendable1)).supportsInterface(customInterface2)
+        );
+    }
+
+    function testRemoveSupportForNonExistentInterface() public {
+        extendable1.addExtension(
+            SUPPORTS_INTERFACE_SELECTOR,
+            address(erc165Extension)
+        );
+
+        bytes4 nonExistentInterface = bytes4(
+            keccak256("nonExistentInterface()")
+        );
+
+        extendable1.execute(
+            address(erc165Extension),
+            abi.encodeWithSelector(
+                REMOVE_SUPPORT_SELECTOR,
+                nonExistentInterface
+            )
+        );
+
+        assertFalse(
+            IERC165(address(extendable1)).supportsInterface(
+                nonExistentInterface
+            )
+        );
+    }
+
+    function testAddSupportTwice() public {
+        extendable1.addExtension(
+            SUPPORTS_INTERFACE_SELECTOR,
+            address(erc165Extension)
+        );
+
+        extendable1.execute(
+            address(erc165Extension),
+            abi.encodeWithSelector(ADD_SUPPORT_SELECTOR, ERC165_INTERFACE_ID)
+        );
+        extendable1.execute(
+            address(erc165Extension),
+            abi.encodeWithSelector(ADD_SUPPORT_SELECTOR, ERC165_INTERFACE_ID)
+        );
+
+        assertTrue(
+            IERC165(address(extendable1)).supportsInterface(ERC165_INTERFACE_ID)
+        );
+    }
+
+    function testRemoveSupportTwice() public {
+        extendable1.addExtension(
+            SUPPORTS_INTERFACE_SELECTOR,
+            address(erc165Extension)
+        );
+
+        extendable1.execute(
+            address(erc165Extension),
+            abi.encodeWithSelector(ADD_SUPPORT_SELECTOR, ERC165_INTERFACE_ID)
+        );
+        extendable1.execute(
+            address(erc165Extension),
+            abi.encodeWithSelector(REMOVE_SUPPORT_SELECTOR, ERC165_INTERFACE_ID)
+        );
+        extendable1.execute(
+            address(erc165Extension),
+            abi.encodeWithSelector(REMOVE_SUPPORT_SELECTOR, ERC165_INTERFACE_ID)
+        );
+
+        assertFalse(
+            IERC165(address(extendable1)).supportsInterface(ERC165_INTERFACE_ID)
+        );
+    }
+
+    function testAddAndRemoveMultipleInterfaces() public {
+        extendable1.addExtension(
+            SUPPORTS_INTERFACE_SELECTOR,
+            address(erc165Extension)
+        );
+
+        bytes4[] memory interfaces = new bytes4[](3);
+        interfaces[0] = bytes4(keccak256("interface1()"));
+        interfaces[1] = bytes4(keccak256("interface2()"));
+        interfaces[2] = bytes4(keccak256("interface3()"));
+
+        for (uint i = 0; i < interfaces.length; i++) {
+            extendable1.execute(
+                address(erc165Extension),
+                abi.encodeWithSelector(ADD_SUPPORT_SELECTOR, interfaces[i])
+            );
+            assertTrue(
+                IERC165(address(extendable1)).supportsInterface(interfaces[i])
+            );
+        }
+
+        for (uint i = 0; i < interfaces.length; i++) {
+            extendable1.execute(
+                address(erc165Extension),
+                abi.encodeWithSelector(REMOVE_SUPPORT_SELECTOR, interfaces[i])
+            );
+            assertFalse(
+                IERC165(address(extendable1)).supportsInterface(interfaces[i])
+            );
+        }
+    }
+
+    function testCrossExtendableInterference() public {
+        extendable1.addExtension(
+            SUPPORTS_INTERFACE_SELECTOR,
+            address(erc165Extension)
+        );
+        extendable2.addExtension(
+            SUPPORTS_INTERFACE_SELECTOR,
+            address(erc165Extension)
+        );
+
+        bytes4 customInterface = bytes4(keccak256("customInterface()"));
+
+        extendable1.execute(
+            address(erc165Extension),
+            abi.encodeWithSelector(ADD_SUPPORT_SELECTOR, customInterface)
+        );
+
+        assertTrue(
+            IERC165(address(extendable1)).supportsInterface(customInterface)
+        );
+        assertFalse(
+            IERC165(address(extendable2)).supportsInterface(customInterface)
+        );
+
+        extendable2.execute(
+            address(erc165Extension),
+            abi.encodeWithSelector(REMOVE_SUPPORT_SELECTOR, customInterface)
+        );
+
+        assertTrue(
+            IERC165(address(extendable1)).supportsInterface(customInterface)
+        );
+        assertFalse(
+            IERC165(address(extendable2)).supportsInterface(customInterface)
+        );
+    }
+}
